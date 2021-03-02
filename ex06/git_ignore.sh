743d3bea490839962f9ec13e0f1344a3e849c705
Ralf Lacresha <rlacresh@si-k6.kzn.21-school.ru>
2021-03-02 08:45:27 +0300

    ex06 done

---
+++ ex06/git_ignore.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+#script that provides existing files ignored by Ralf Lacresha's GiT
+
+git clone https://github.com/huuga/PiscineShell00.git &> /dev/null
+cd PiscineShell00
+cat .gitignore
